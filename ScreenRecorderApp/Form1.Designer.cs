57099ea6a31ddddba264f856f889cfb2842145c6
samshy87 <120481195+samshy87@users.noreply.github.com>
2022-12-13 10:34:10 -0600

    Add files via upload

---
+++ ScreenRecorderApp/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace ScreenRecordApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.recordStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // recordStart
+            // 
+            this.recordStart.Location = new System.Drawing.Point(183, 78);
+            this.recordStart.Name = "recordStart";
+            this.recordStart.Size = new System.Drawing.Size(104, 42);
+            this.recordStart.TabIndex = 0;
+            this.recordStart.Text = "Start Recording";
+            this.recordStart.UseVisualStyleBackColor = true;
+            this.recordStart.Click += new System.EventHandler(this.recordStart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(201, 139);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Recording...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(467, 261);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.recordStart);
+            this.Name = "Form1";
+            this.Text = "Screen Record Application";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button recordStart;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ScreenRecorderApp/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+
+namespace ScreenRecordApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.FormClosing += new FormClosingEventHandler(Form1_FormClosing);
+        }
+
+        public int onOff = 0;
+        Process cmd = new Process();
+        private void recordStart_Click(object sender, EventArgs e)
+        {
+            if (onOff == 0)
+            {
+                onOff = 1;
+                recordStart.Text = "Stop Recording";
+                label1.Visible = true;
+                string strCmdText;
+                strCmdText = "ffmpeg -framerate 6 -f gdigrab -i desktop -c:v libx264 -pix_fmt yuv420p C:\\Users\\shysa\\Desktop\\TestRecordings\\test" + DateTime.Now.ToString("HH-mm-ss") + ".avi";
+                cmd.StartInfo.FileName = "cmd.exe";
+                cmd.StartInfo.RedirectStandardInput = true;
+                cmd.StartInfo.RedirectStandardOutput = true;
+                cmd.StartInfo.CreateNoWindow = true;
+                cmd.StartInfo.UseShellExecute = false;
+                cmd.Start();
+                cmd.StandardInput.WriteLine(strCmdText);
+            }
+            else if (onOff == 1)
+            {
+                onOff = 0;
+                label1.Visible = false;
+                recordStart.Text = "Start Recording";
+                cmd.StandardInput.WriteLine("q");
+            }
+
+        }
+        public void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            cmd.StandardInput.WriteLine("q");
+        }
+
+    }
+}
